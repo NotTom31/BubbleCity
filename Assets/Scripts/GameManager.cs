f73ea7db43b15e94ccb279cd55b7f75ad154e87d
lestherreynoso <lhreynos@gmail.com>
2025-01-25 11:21:22 -0800

    Adding Ship Logistics to the Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,7 +14,9 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public GameState gameState { get; private set; } = GameState.MAIN_MENU;
-
+    public ShipLogistics Logistics = new ShipLogistics();
+    
+    
     // Start is called before the first frame update
     void Start()
     {
