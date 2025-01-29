f72b5bb7e8817ad331efe77c1ab0a0f069ea850f
Thomas Mezzie <99389013+NotTom31@users.noreply.github.com>
2025-01-29 14:47:57 -0800

    Implement credits

--- Assets/Scripts/UI/TitleMenuController.cs
+++ Assets/Scripts/UI/TitleMenuController.cs
@@ -11,12 +11,16 @@ public class TitleMenuController : MonoBehaviour
     public Button StartButton;
     public Button CreditsButton;
     public Button QuitButton;
+    public Button BackButton;
+    public GameObject CreditsUI;
 
     private void Start()
     {
         StartButton.onClick.AddListener(StartGame);
         CreditsButton.onClick.AddListener(ShowCredits);
         QuitButton.onClick.AddListener(QuitGame);
+        BackButton.onClick.AddListener(BackToMenu);
+        CreditsUI.SetActive(false);
     }
 
     private void StartGame()
@@ -29,6 +33,12 @@ public class TitleMenuController : MonoBehaviour
     private void ShowCredits()
     {
         Debug.Log("Show Credits");
+        CreditsUI.SetActive(true);
+    }
+
+    private void BackToMenu()
+    {
+        CreditsUI.SetActive(false);
     }
     
     private void QuitGame()
